e3f9b6525634506d4b3a0b98c307b935ad00cebd
teguhpermadi <tguh.permadi@gmail.com>
2024-10-29 11:48:01 +0700

    wip: style print preview

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class PrintController extends Controller
@@ -10,6 +11,8 @@ class PrintController extends Controller
     public function print_preview($id)
     {
         $student = Student::find($id);
+        $pdf = Pdf::loadView('print-preview', ['student' => $student]);
+        // return $pdf->download();
         // dd($student);
         return view('print-preview', ['student' => $student]);
     }
--- resources/views/print-preview.blade.php
+++ resources/views/print-preview.blade.php
@@ -1,209 +1,226 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Print Preview</title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('css/pdf.css') }}" type="text/css">
 </head>
+
 <body>
-    <h1>Formulir Pendaftaran Peserta Didik Baru</h1>
-    <h2>MI AR RIDLO</h2>
-    <h3>Tahun Pelajaran : 2025/2026</h3>
+    <img src="{{ asset('image/logo.png') }}" alt="logo" height="100" class="center-img">
+    <h1 class="center">Formulir Pendaftaran Peserta Didik Baru</h1>
+    <h2 class="center">MI AR RIDLO</h2>
+    <h3 class="center">Tahun Pelajaran : 2025/2026</h3>
     <hr>
-    <h4>Identitas Siswa</h4>
     <table>
+        <tr>
+            <td colspan="3">
+                <h4>Identitas Siswa</h4>
+            </td>
+        </tr>
         <tr>
             <td>Nama Lengkap</td>
             <td>:</td>
-            <td>{{$student->full_name}}</td>
+            <td>{{ $student->full_name }}</td>
         </tr>
         <tr>
             <td>Nama Panggilan</td>
             <td>:</td>
-            <td>{{$student->nick_name}}</td>
+            <td>{{ $student->nick_name }}</td>
         </tr>
         <tr>
             <td>Jenis Kelamin</td>
             <td>:</td>
-            <td>{{$student->gender}}</td>
+            <td>{{ $student->gender }}</td>
         </tr>
         <tr>
             <td>Tempat Lahir</td>
             <td>:</td>
-            <td>{{$student->city_born}}</td>
+            <td>{{ $student->city_born }}</td>
         </tr>
         <tr>
             <td>Tanggal Lahir</td>
             <td>:</td>
-            <td>{{$student->birthday}}</td>
+            <td>{{ $student->birthday }}</td>
         </tr>
         <tr>
             <td>NISN</td>
             <td>:</td>
-            <td>{{$student->nisn}}</td>
+            <td>{{ $student->nisn }}</td>
         </tr>
         <tr>
             <td>NIK</td>
             <td>:</td>
-            <td>{{$student->nik}}</td>
+            <td>{{ $student->nik }}</td>
         </tr>
-    </table>
-
-    <h4>Alamat</h4>
-    <table>
+        <tr>
+            <td colspan="3">
+                <h4>Alamat</h4>
+            </td>
+        </tr>
         <tr>
             <td>Alamat</td>
             <td>:</td>
-            <td>{{$student->address}}</td>
+            <td>{{ $student->address }}</td>
         </tr>
         <tr>
             <td>Kelurahan</td>
             <td>:</td>
-            <td>{{$student->village}}</td>
+            <td>{{ $student->village }}</td>
         </tr>
         <tr>
             <td>Kecamatan</td>
             <td>:</td>
-            <td>{{$student->district}}</td>
+            <td>{{ $student->district }}</td>
         </tr>
         <tr>
             <td>Kota</td>
             <td>:</td>
-            <td>{{$student->city}}</td>
+            <td>{{ $student->city }}</td>
         </tr>
         <tr>
             <td>Provinsi</td>
             <td>:</td>
-            <td>{{$student->province}}</td>
+            <td>{{ $student->province }}</td>
         </tr>
         <tr>
             <td>Kode Pos</td>
             <td>:</td>
-            <td>{{$student->poscode}}</td>
+            <td>{{ $student->poscode }}</td>
         </tr>
-    </table>
-
-    <h4>Identitas Ayah</h4>
-    <table>
+        <tr></tr>
+        <tr>
+            <td colspan="3">
+                <h4>Identitas Ayah</h4>
+            </td>
+        </tr>
         <tr>
             <td>Status</td>
             <td>:</td>
-            <td>{{$student->father_status}}</td>
+            <td>{{ $student->father_status }}</td>
         </tr>
         <tr>
             <td>NIK Ayah</td>
             <td>:</td>
-            <td>{{$student->father_nik}}</td>
+            <td>{{ $student->father_nik }}</td>
         </tr>
         <tr>
             <td>Nama Ayah</td>
             <td>:</td>
-            <td>{{$student->father_name}}</td>
+            <td>{{ $student->father_name }}</td>
         </tr>
         <tr>
             <td>Tempat Lahir Ayah</td>
             <td>:</td>
-            <td>{{$student->father_city_born}}</td>
+            <td>{{ $student->father_city_born }}</td>
         </tr>
         <tr>
             <td>Tanggal Lahir Ayah</td>
             <td>:</td>
-            <td>{{$student->father_birthday}}</td>
+            <td>{{ $student->father_birthday }}</td>
         </tr>
         <tr>
             <td>Agama Ayah</td>
             <td>:</td>
-            <td>{{$student->father_religon}}</td>
+            <td>{{ $student->father_religon }}</td>
         </tr>
         <tr>
             <td>Pendidikan Ayah</td>
             <td>:</td>
-            <td>{{$student->father_education}}</td>
+            <td>{{ $student->father_education }}</td>
         </tr>
         <tr>
             <td>Hubungan Ayah dengan anak</td>
             <td>:</td>
-            <td>{{$student->father_relation}}</td>
+            <td>{{ $student->father_relation }}</td>
         </tr>
         <tr>
             <td>Pekerjaan Ayah</td>
             <td>:</td>
-            <td>{{$student->father_job}}</td>
+            <td>{{ $student->father_job }}</td>
         </tr>
         <tr>
             <td>Penghasilan Ayah</td>
             <td>:</td>
-            <td>{{$student->father_income}}</td>
+            <td>{{ $student->father_income }}</td>
         </tr>
         <tr>
             <td>Telp Ayah</td>
             <td>:</td>
-            <td>{{$student->father_phone}}</td>
+            <td>{{ $student->father_phone }}</td>
         </tr>
-    </table>
-
-    <h4>Identitas Ibu</h4>
-    <table>
+        <tr>
+            <td colspan="3">
+                <h4>Identitas Ibu</h4>
+            </td>
+        </tr>
         <tr>
             <td>Status</td>
             <td>:</td>
-            <td>{{$student->mother_status}}</td>
+            <td>{{ $student->mother_status }}</td>
         </tr>
         <tr>
             <td>NIK Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_nik}}</td>
+            <td>{{ $student->mother_nik }}</td>
         </tr>
         <tr>
             <td>Nama Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_name}}</td>
+            <td>{{ $student->mother_name }}</td>
         </tr>
         <tr>
             <td>Tempat Lahir Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_city_born}}</td>
+            <td>{{ $student->mother_city_born }}</td>
         </tr>
         <tr>
             <td>Tanggal Lahir Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_birthday}}</td>
+            <td>{{ $student->mother_birthday }}</td>
         </tr>
         <tr>
             <td>Agama Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_religon}}</td>
+            <td>{{ $student->mother_religon }}</td>
         </tr>
         <tr>
             <td>Pendidikan Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_education}}</td>
+            <td>{{ $student->mother_education }}</td>
         </tr>
         <tr>
             <td>Hubungan Ibu dengan anak</td>
             <td>:</td>
-            <td>{{$student->mother_relation}}</td>
+            <td>{{ $student->mother_relation }}</td>
         </tr>
         <tr>
             <td>Pekerjaan Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_job}}</td>
+            <td>{{ $student->mother_job }}</td>
         </tr>
         <tr>
             <td>Penghasilan Ibu</td>
             <td>:</td>
-            <td>{{ $student->mother_income}}</td>
+            <td>{{ $student->mother_income }}</td>
         </tr>
         <tr>
             <td>Telp Ibu</td>
             <td>:</td>
-            <td>{{$student->mother_phone}}</td>
+            <td>{{ $student->mother_phone }}</td>
         </tr>
     </table>
 
-    <p>Malang, {{$student->created_at->format('j F Y')}}</p>
-    <img src="{{ asset('storage/' . $student->ttd) }}" alt="Signature" height="40">
-    <p>{{$student->ttd_name}}</p>
+    <div class="center">
+        <p>Malang, {{ $student->created_at->format('j F Y') }}</p>
+        <img src="{{ asset('storage/' . $student->ttd) }}" alt="Signature" height="40">
+        <p>{{ $student->ttd_name }}</p>
+    </div>
 </body>
+
 </html>
